a37a801aff7bc3b86b64ea59ee7b8e4e1d0476e8
Christopher R Robles <crrobles2@gmail.com>
2022-03-04 17:53:55 -0800

    Update snpEFF.sh

--- Source Code/snpEFF.sh
+++ Source Code/snpEFF.sh
@@ -1,1 +1,2 @@
-java -Xmx8g -jar snpEff.jar -o bed -v -csvStats ex1.csv GRCh37.75 ../all.pheno.all.credible.covar.vcf > Data\ S8.txt
+#this is the snpEFF script to make the Data S8 file.  A VCF for all the credible nims must be created first.
+java -Xmx8g -jar snpEff.jar -o bed -v -csvStats ex1.csv GRCh37.75 ./all.pheno.all.credible.covar.vcf > Data\ S8.txt
